b57ab4009000ff0faabd8dee503913150686b9af
Aaron Snowberger <jekkilekki@gmail.com>
2017-01-03 17:00:23 +0900

    Rearrange sections

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
           echo "<p class='huge-number'>$courses</p>";
           echo "<p>";
           for( $i = 0; $i < $courses; $i++ ) {
-            echo "<i class='fa fa-graduation-cap'></i>&nbsp;";
+            echo "<i class='fa fa-graduation-cap'></i> ";
           }
           echo "</p>";
           ?>
@@ -153,6 +153,50 @@
           </div>
           </div>
         </article>
+        <article class="audible">
+          <h3 class="article-title">Books "Read"</h3>
+          <p>I do most of my "reading" via the Audible app on my 1 hour work commute.</p>
+          <?php
+          $books = 26;
+          echo "<p class='huge-number'>$books</p>";
+          echo "<p>";
+          for( $i = 0; $i < $books; $i++ ) {
+            echo "<i class='fa fa-2x fa-book'></i> ";
+          }
+          echo "</p>";
+          ?>
+        </article>
+      </div><!-- .section-content -->
+      </div><!-- .row -->
+      </section>
+      <section>
+        <div class="row">
+        <aside class="section-aside stick-it">
+          <h1 class="section-title">Coding</h1>
+          <h2 class="section-subtitle">The Year of PHP</h2>
+          <p>
+            Hi! My name's Aaron Snowberger and I'm an WordPress/PHP web developer
+            who's living in Jeonju, South Korea. I teach ESL at <a href="">Jeonju University</a>,
+            Computer Science and Graphic Design at <a href="">GPA International High School</a>,
+            and do freelance work with <a href="">various clients</a> to help them achieve their
+            objectives on the web and in design. This is a Review of my Year: 2016.
+          </p>
+          <p>
+            I'm passionate about making the web a better place for everyone (<a href="">South
+            Korean internet</a> drove me to that passion in the first place).
+            To that end, I strive to create websites and projects that are beautiful,
+            intuitive, and have a great user experience. I work hard to stick to
+            well-established coding standards, use loads of comments, and document
+            everything. With design, I strive to make things that are not only
+            visually pleasing, but also communicate the design objectives effectively
+            to their desired audience.
+          </p>
+          <p>
+            In 2016, I made a number of key decisions that helped propel those visions
+            forward. Two of the biggest were rejoining Lynda.com and
+          </p>
+        </aside>
+        <div class="section-content">
         <article class="wp-contributions">
           <h3 class="article-title">Contributions to WordPress</h3>
           <div class="row">
@@ -172,44 +216,73 @@
         </article>
         <article class="wp-contributions">
           <h3 class="article-title">Contributions to GitHub</h3>
-          <img class="github-calendar-full" src="http://ghchart.rshah.org/2a4170/jekkilekki" alt="2016rshah's Github chart" />
+          <!-- <p>485 contributions in the last year</p> -->
+          <img class="github-calendar-full" src="images/github-contrib-2016-color.png" alt="Jekkilekki's Github chart" />
+          <!-- <img class="github-calendar-full" src="http://ghchart.rshah.org/2a4170/jekkilekki" alt="2016rshah's Github chart" /> -->
           <!-- Prepare a container for your calendar. -->
-          <div class="github-calendar">
+          <!-- <div class="github-calendar"> -->
               <!-- Loading stuff -->
-              Loading GitHub data.
+              <!-- Loading GitHub data. -->
+          <!-- </div> -->
+          <!-- <div class="clear"></div> -->
+          <div class="row">
+            <div class="col-3">
+              <h3 class="article-title">Contributions last year</h3>
+              <p class="large-number">485<span> total</span></p>
+              <p class="article-description">Jan 1, 2016 - Jan 1, 2017</p>
+            </div>
+            <div class="col-3">
+              <h3 class="article-title">Longest streak</h3>
+              <p class="large-number">64<span> days</span></p>
+              <p class="article-description">March 26 - May 28</p>
+            </div>
+            <div class="col-3">
+              <h3 class="article-title">Current streak</h3>
+              <p class="large-number">0<span> days</span></p>
+              <p class="article-description">Last contributed Dec 31, 2016</p>
+            </div>
           </div>
-          <div class="clear"></div>
         </article>
         <article class="websites">
           <h3 class="article-title">New Sites Built</h3>
 
         </article>
         <article class="audible">
-          <h3 class="article-title">Books "Read"</h3>
-          <p>I do most of my "reading" via the Audible app on my 1 hour work commute.</p>
-          <?php
-          $books = 26;
-          echo "<p class='huge-number'>$books</p>";
-          echo "<p>";
-          for( $i = 0; $i < $books; $i++ ) {
-            echo "<i class='fa fa-2x fa-book'></i>&nbsp;";
-          }
-          echo "</p>";
-          ?>
-        </article>
-        <article class="audible">
           <h3 class="article-title">Books Designed &amp; Published</h3>
-
+          <div class="row">
+            <div class="col-4">
+              <img src="">
+              <p class="article-description">Something</p>
+            </div>
+            <div class="col-4">
+              <img src="">
+              <p class="article-description">Something</p>
+            </div>
+            <div class="col-4">
+              <img src="">
+              <p class="article-description">Something</p>
+            </div>
+            <div class="col-4">
+              <img src="">
+              <p class="article-description">Something</p>
+            </div>
+          </div>
         </article>
         <article class="audible row">
           <div class="col-3">
             <h3 class="article-title">KM Cycled</h3>
+            <p class="large-number">4,000</p>
+            <p class="article-description">Something</p>
           </div>
           <div class="col-3">
             <h3 class="article-title">Protein Consumed</h3>
+            <p class="large-number">1,875g</p>
+            <p class="article-description">Something</p>
           </div>
           <div class="col-3">
             <h3 class="article-title">Weight Lost</h3>
+            <p class="large-number">5kg</p>
+            <p class="article-description">Something</p>
           </div>
         </article>
         <article class="audible">
